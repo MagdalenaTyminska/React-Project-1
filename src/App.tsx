f4bef7b68956644c9e55fc3518d4fefb893e8018
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-10-31 21:21:04 +0100

    removed duplicated component to retrigger deployment

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
 		<>
 			<Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
 			<Statistics title='Upload stats' stats={data} />
-			<Statistics title='Upload stats' stats={data} />
 			<FriendsList friends={friends} />
 			<Transactions items={items} />
 		</>
