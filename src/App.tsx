f3e8d76eca2ce6f7c345a2c9e60422e6683ab1f5
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-10-31 13:39:25 +0100

    added component - Statistics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import user from './components/Profile/user.json';
 import { Profile } from './components/Profile/Profile';
+import data from './components/Statistics/data.json';
+import { Statistics } from './components/Statistics/Statistics';
 
 function App() {
 	return (
 		<>
 			<Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+			<Statistics title='Upload stats' stats={data} />
 		</>
 	);
 }
