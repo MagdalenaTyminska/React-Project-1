0ce8637a58cd22c9ff52f32844466b745572df1e
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-10-31 21:55:45 +0100

    added base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: 'https://magdalenatyminska.github.io/React-Project-1/',
 	server: {
 		port: 5000,
 	},
