b5b3185036dfe5bd8a76c6d174783af338ad6335
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-10-31 13:12:35 +0100

    added component -Profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import user from './components/Profile/user.json';
+import { Profile } from './components/Profile/Profile';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+		</>
+	);
 }
 
 export default App;
