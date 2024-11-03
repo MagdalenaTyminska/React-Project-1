400e3dffda5fc852cc00890ef7273a6caae768a2
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2024-11-03 21:26:34 +0100

    added FriendsList

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import user from './components/Profile/profile.json';
 import { Profile } from './components/Profile/Profile';
 import data from './components/Statistics/statistics.json';
 import { Statistics } from './components/Statistics/Statistics';
+import friends from './components/FriendsList/friends.json';
+import { FriendsList } from './components/FriendsList/FriendsList';
 
 function App() {
 	return (
 		<>
 			<Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
 			<Statistics title='Upload stats' stats={data} />
+			<FriendsList friends={friends} />
 		</>
 	);
 }
